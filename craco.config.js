3c404a500c6a8c018443506ee77d9ba56a5c91f2
jesusrpd <jesusrodrigo881@gmail.com>
2021-05-21 09:31:59 -0500

    Tailwind css include and configuration proyect homeworks app from add styles

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+//Craco config
+module.exports = {
+    style: {
+        postcss: {
+            plugins:[
+                require('tailwindcss'),
+                require('autoprefixer')
+            ]
+        }
+    }
+};
